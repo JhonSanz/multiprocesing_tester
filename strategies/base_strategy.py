0d30a907be5dbf27aa46f487f84f569f20116880
JhonSanz <ingjhonsanz@utp.edu.co>
2022-10-26 10:20:54 -0500

    Strategy done. Let's test it

--- strategies/base_strategy.py
+++ strategies/base_strategy.py
@@ -9,23 +9,52 @@ class BaseStrategy:
     def __init__(self):
         self.orders = []
     
-    def open_operation(self, price, date, type):
+    def open_operation(self, price, date, type, stop_loss=None):
         self.orders.append({
             "price_open": price, "date_open": date, "type": type,
-            "date_close": None, "price_close": None, "direction": self._IN
+            "date_close": None, "price_close": None, "direction": self._IN,
+            "stop_loss": stop_loss, "comment": ""
         })
+        return len(self.orders)
 
-    def close_operation(self, date_opened, date_close, price_close):
+    def close_operation(self, date_opened, date_close, price_close, comment=""):
         position = list(filter(
-            lambda x: x["date_open"] == date_opened, self.orders))[0]
+            lambda x: (
+                x["date_open"] == date_opened and
+                x["date_close"] is None and
+                x["price_close"] is None
+            ), self.orders
+        ))[0]
         item_pos = self.orders.index(position)
         position = {
             **position, "date_close": date_close,
             "price_close": price_close,
-            "direction": self._OUT
+            "direction": self._OUT,
+            "comment": comment
         }
         self.orders[item_pos] = position
     
-    def save_orders(self):
+    def validate_stop_losses(self, current_date, current_price):
+        orders = list(filter(
+            lambda x: (
+                x["direction"] == self._IN and
+                x["stop_loss"] is not None and
+                x["price_close"] is None and
+                x["date_close"] is None and (
+                    x["stop_loss"] <= current_price if x["type"] == self.BUY else
+                    x["stop_loss"] >= current_price
+                )
+            ), self.orders
+        ))
+        for operation in orders:
+            self.close_operation(
+                operation["date_open"], current_date, current_price,
+                f"Stop loss at {current_price}"
+            )
+
+    def get_position_by_ticket(self, ticket):
+        return self.orders[ticket]
+
+    def save_orders(self, filename):
         df = pd.DataFrame(data=self.orders)
-        df.to_csv("orders.csv")
+        df.to_csv(f"orders/{filename}_orders.csv")
--- strategies/two_means/two_sma.py
+++ strategies/two_means/two_sma.py
@@ -9,29 +9,53 @@ class TwoMeansStrategy(BaseStrategy):
     def __init__(self, data):
         super().__init__()
         self.data = data
-        self.high = "sma_[\d]+_0"
-        self.low = "sma_[\d]+_1"
+        self.high_label = ""
+        self.low_label = ""
 
     def get_column_names(self, pattern):
         columns = self.data.columns
         p = re.compile(pattern)
-        return list(filter(lambda y: y is not None, map(lambda x: p.search(x), columns)))[0].group(0)
+        return list(filter(
+            lambda y: y is not None,
+            map(lambda x: p.search(x), columns)
+        ))[0].group(0)
 
     def run(self):
-        high_label = self.get_column_names(self.high)
-        low_label = self.get_column_names(self.low)
+        self.high_label = self.get_column_names("sma_[\d]+_0")
+        self.low_label = self.get_column_names("sma_[\d]+_1")
         _data = self.data.copy()
         _data.loc[:, "trend"] = 0
-        _data.loc[_data["close"] >= _data[f"{high_label}"], ["trend"]] = self.UP_TREND
-        _data.loc[_data["close"] <= _data[f"{low_label}"], ["trend"]] = self.DOWN_TREND
+        _data.loc[_data["close"] >= _data[f"{self.high_label}"], ["trend"]] = self.UP_TREND
+        _data.loc[_data["close"] <= _data[f"{self.low_label}"], ["trend"]] = self.DOWN_TREND
+
+        opened_position = False
+        ticket = 0
         for (
             date, close, sma_high, sma_low, trend
         ) in zip(
-            _data["date"], _data["close"], _data[f"{high_label}"],
-            _data[f"{low_label}"], _data["trend"]
+            _data["date"], _data["close"], _data[f"{self.high_label}"],
+            _data[f"{self.low_label}"], _data["trend"]
         ):
-            if (close < sma_low and trend == self.UP_TREND):
-                self.open_operation(close, date, self.SELL)
-            if (close > sma_high and trend == self.DOWN_TREND):
-                self.open_operation(close, date, self.SELL)
-
+            if not opened_position:
+                if (close < sma_low and trend == self.UP_TREND):
+                    ticket = self.open_operation(close, date, self.SELL)
+                    opened_position = True
+                if (close > sma_high and trend == self.DOWN_TREND):
+                    ticket = self.open_operation(close, date, self.SELL)
+                    opened_position = True
+            else:
+                self.validate_stop_losses(date, close)
+                pos_info = self.get_position_by_ticket(ticket)
+                if (
+                    (
+                        pos_info["type"] == self.SELL
+                        and trend == self.DOWN_TREND
+                        and close > sma_low
+                    ) or (
+                        pos_info["type"] == self.BUY
+                        and trend == self.UP_TREND
+                        and close < sma_high
+                    )
+                ):
+                    self.close_operation(pos_info["date_open"], date, close)
+        self.save_orders(f"{self.high_label}_{self.low_label}")
