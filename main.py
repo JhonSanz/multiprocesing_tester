0c107c447237caf186a773d6f2ec0ae35ec78050
JhonSanz <ingjhonsanz@utp.edu.co>
2022-10-27 14:02:35 -0500

    Added messages

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from get_data import DataGetter
 from indicators import Indicators
 from strategies.two_means.two_sma import TwoMeansStrategy
 from statistics import Statistics
+from termcolor import colored
 
 def run_strategy(config_value, indicators_params, strategy_params):
+    print(colored(f"Preparando data para {'_'.join(map(str, config_value))}", "green"))
     data_getter = DataGetter(strategy_params)
+    print(colored("Calculando indicadores", "yellow"))
     data_indicators = Indicators(
         config_value,
         indicators_params,
         data_getter.data
     ).choose_algorithms()
     # data_indicators.to_csv("test_data_indicators.csv", index=False)
+    print(colored("Calculando estrategia", "yellow"))
     orders = TwoMeansStrategy(data_indicators).run()
     orders.dropna(inplace=True)
     result = Statistics(orders, config_value).run()
