b5667b61e88fce0c6e9b4edf17d168181c45d091
JhonSanz <ingjhonsanz@utp.edu.co>
2022-11-02 09:45:05 -0500

    Added termcolor

--- commands.py
+++ commands.py
@@ -1,60 +1,54 @@
-import sys
 import os
 from shutil import rmtree
-
 import argparse
+from termcolor import colored
 
 parser = argparse.ArgumentParser(
     description='''------- File with useful commands -------''',
     epilog="""---------------- Bye bye ----------------"""
 )
 parser.add_argument(
-    'reset_all', type=str, default='reset_all',
-    help='Deletes all folders'
+    '-o',
+    type=str,
+    choices=['reset_all', 'reset_orders', 'reset_totals', 'reset_results'],
+    default='reset_all', required=True,
+    help='Option to delete some files'
 )
-parser.add_argument(
-    'reset_totals', type=str, default='reset_totals',
-    help='Deletes totals folder'
-)
-parser.add_argument(
-    'reset_orders', type=str, default='reset_orders',
-    help='Deletes orders folder'
-)
-parser.add_argument(
-    'reset_results', type=str, default='reset_results',
-    help='Deletes results file'
-)
-args=parser.parse_args()
-
+args = parser.parse_args()
 
 def reset_totals():
     if os.path.exists("totals"):
         rmtree("totals")
+        print(colored("Totals dir deleted", "green"))
     if not os.path.exists('totals'):
         os.makedirs('totals')
 
 def reset_orders():
     if os.path.exists("orders"):
         rmtree("orders")
+        print(colored("Orders dir deleted", "green"))
     if not os.path.exists('orders'):
         os.makedirs('orders')
 
 def reset_results():
     try:
         os.remove("results.csv")
+        print(colored("Results file deleted", "green"))
     except FileNotFoundError:
         pass
 
 def reset_all():
-    reset_totals()
-    reset_orders()
-    reset_results()
+    value = input("Are you sure you want to delete all? (y/n): ")
+    if (value == "y"):
+        reset_totals()
+        reset_orders()
+        reset_results()
 
-if (sys.argv[1] == "reset_all"):
+if (args.o == "reset_all"):
     reset_all()
-if (sys.argv[1] == "reset_orders"):
+if (args.o == "reset_orders"):
     reset_orders()
-if (sys.argv[1] == "reset_results"):
+if (args.o == "reset_results"):
     reset_results()
-if (sys.argv[1] == "reset_totals"):
+if (args.o == "reset_totals"):
     reset_totals()
