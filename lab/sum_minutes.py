5cdeb8dccf673802fa0868c09e9e017297800703
JhonSanz <ingjhonsanz@utp.edu.co>
2022-11-16 16:21:40 -0500

    Added sum minutes program.

---
+++ lab/sum_minutes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+df = pd.read_csv("totals/715_715_totals.csv", index_col=[0])
+df['date_open'] = pd.to_datetime(df['date_open'], format='%Y-%m-%d %H:%M')
+df['date_open'] = df['date_open'] + timedelta(minutes=10)
+df['date_open'] = df['date_open'].dt.strftime('%Y.%m.%d %H:%M:%S')
+
+df['date_close'] = pd.to_datetime(df['date_close'], format='%Y-%m-%d %H:%M:%S')
+df['date_close'] = df['date_close'] + timedelta(minutes=10)
+df['date_close'] = df['date_close'].dt.strftime('%Y.%m.%d %H:%M:%S')
+df.to_csv("test.csv")
